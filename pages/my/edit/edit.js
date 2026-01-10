db24dd180877f665acb55962454d5394c0dcae09
尖尖 <12165506+zhujian2022@user.noreply.gitee.com>
2026-01-10 13:39:17 +0800

    优化个人信息页面

--- pages/my/edit/edit.js
+++ pages/my/edit/edit.js
@@ -11,7 +11,8 @@ Page({
    */
   data: {
     nickName: '',
-    showDialog: false
+    isEditing: false, // 是否正在编辑
+    editingName: '' // 编辑中的昵称
   },
 
   /**
@@ -31,91 +32,140 @@ Page({
       console.log(this.data.nickName)
     }
   },
-  onNickNameChange(e) {
+  // 开始编辑昵称
+  startEditNickName() {
     this.setData({
-      nickName: e.detail.value
+      isEditing: true,
+      editingName: this.data.nickName || ''
     })
-  },
-  //复制userId
-  onUserIdCopy() {
-    this.onCopy(this.userId)
-  },
-  //复制pwd
-  onPwdCopy() {
-    this.onCopy(this.pwd)
-  },
-  onCopy(data) {
-    wx.setClipboardData({
-      data: data,
-      success: function () {
-        wx.showToast({
-          title: '复制成功',
-          icon: 'success',
-          duration: 2000
-        });
-      },
-      fail: function (res) {
-        wx.showToast({
-          title: '复制失败：' + res.errMsg,
-          icon: 'none',
-          duration: 2000
-        });
-      },
-      complete: function () {
-        console.log('复制操作结束');
-      }
-    });
-  },
-  showNickNameDialog() {
-    this.setData({
-      showDialog: true,
-      name: this.data.nickName
-    })
-  },
-  closeDialog(e) {
-    if (e.type === 'confirm') {
-      if (this.data.name) {
-        this.onSave()
-      }
-    } else {
+    // 聚焦输入框
+    setTimeout(() => {
       this.setData({
-        showDialog: false
+        focusInput: true
       })
-    }
+    }, 100)
   },
-  onNameChange(e) {
-    console.log(e.detail.value)
+  // 输入框值变化
+  onNickNameInput(e) {
+    const value = e.detail.value || ''
     this.setData({
-      name: e.detail.value
+      editingName: value
     })
+  },
+  // 取消编辑
+  cancelEdit() {
+    this.setData({
+      isEditing: false,
+      editingName: '',
+      focusInput: false
+    })
+  },
+  // 保存昵称
+  saveNickName() {
+    const name = (this.data.editingName || '').trim()
+    
+    // 检查是否输入了昵称
+    if (!name) {
+      wx.showToast({
+        title: '请输入昵称',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    
+    // 检查昵称长度
+    if (name.length > 10) {
+      wx.showToast({
+        title: '昵称最多10个字符',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    
+    // 如果昵称没有变化，直接关闭编辑
+    if (name === this.data.nickName) {
+      this.setData({
+        isEditing: false,
+        editingName: '',
+        focusInput: false
+      })
+      return
+    }
+    
+    // 执行保存
+    this.onSave(name)
   },
   //保存昵称
-  onSave() {
+  onSave(name) {
     const param = {
       userId: wx.getStorageSync('userId'),
-      nickName: this.data.name
+      nickName: name
     }
-    msgSecCheck(this.data.name).then(res => {
+    
+    // 内容安全检查
+    msgSecCheck(name).then(res => {
       if (res) {
+        // 内容安全通过，保存昵称
         updateNickName(param).then(res => {
           if (res) {
+            // 保存成功，更新本地缓存
+            let userInfo = wx.getStorageSync('userInfo')
+            if (userInfo) {
+              userInfo.nickName = name
+              wx.setStorageSync('userInfo', userInfo)
+            }
+            
             wx.showToast({
               title: '保存成功',
-              mask: true
+              icon: 'success',
+              mask: true,
+              duration: 2000
             })
+            
             this.setData({
-              nickName: this.data.name,
-              showDialog: false
+              nickName: name,
+              isEditing: false,
+              editingName: '',
+              focusInput: false
             })
           } else {
             wx.showToast({
-              title: '保存失败',
-              icon: 'error',
-              mask: true
+              title: '保存失败，请重试',
+              icon: 'none',
+              mask: true,
+              duration: 2000
             })
+            // 保存失败时保持编辑状态，让用户可以重试
           }
+        }).catch(err => {
+          console.error('保存昵称失败:', err)
+          wx.showToast({
+            title: '保存失败，请重试',
+            icon: 'none',
+            mask: true,
+            duration: 2000
+          })
         })
+      } else {
+        // 内容安全检查未通过
+        wx.showToast({
+          title: '昵称包含敏感内容，请修改',
+          icon: 'none',
+          mask: true,
+          duration: 2000
+        })
+        // 保持编辑状态，让用户可以修改
       }
+    }).catch(err => {
+      console.error('内容安全检查失败:', err)
+      wx.showToast({
+        title: '检查失败，请重试',
+        icon: 'none',
+        mask: true,
+        duration: 2000
+      })
     })
   },
   toLogin() {
