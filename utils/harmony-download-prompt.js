c39f1d77ac1804111cbec9c7e4876dcdab2e9bbb
尖尖 <12165506+zhujian2022@user.noreply.gitee.com>
2026-09-01 19:55:32 +0800

    鸿蒙next手机使用的时候，每次打开都弹出提示框

--- utils/harmony-download-prompt.js
+++ utils/harmony-download-prompt.js
@@ -1,27 +1,13 @@
 const { getDeviceContext } = require('./system-info')
 const { isHarmonyDevice } = require('./device')
 
-const STORAGE_KEY = 'harmonyDownloadPromptDate'
 const HARMONY_DOWNLOAD_PAGE = '/pages/my/app/harmony/harmony'
-
-function getTodayKey() {
-  const now = new Date()
-  const month = String(now.getMonth() + 1).padStart(2, '0')
-  const day = String(now.getDate()).padStart(2, '0')
-  return `${now.getFullYear()}-${month}-${day}`
-}
 
 function tryShowHarmonyDownloadPrompt() {
   if (!isHarmonyDevice(getDeviceContext())) {
     return
   }
 
-  const today = getTodayKey()
-  if (wx.getStorageSync(STORAGE_KEY) === today) {
-    return
-  }
-
-  wx.setStorageSync(STORAGE_KEY, today)
   wx.showModal({
     title: '下载鸿蒙App',
     content: '鸿蒙版小区楼号App已上线，可在华为应用市场搜索「小区楼号」下载安装。',
